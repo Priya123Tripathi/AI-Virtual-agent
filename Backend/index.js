5c93db09c514212f6e7d3a35ddf4194204f0474c
Priya123Tripathi <priyakhejuri01@gmail.com>
2025-12-21 17:44:23 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
- origin:  "https://ai-virtual-agent-f.onrender.com",
+ origin:  "https://ai-virtual-agent-frontend-km91.onrender.com",
  credentials:true
 }
    
